011cd012bca8d4b88ed2dd5b4cfa0b2efecce381
burjuly <51132559+burjuly@users.noreply.github.com>
2022-07-16 14:24:25 +0300

    Delete Day_00/ex00 directory

--- Day_00/ex00/hh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-curl -G -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36'  \
---data-urlencode "text=${1}" \
---data-urlencode  'per_page=20' \
---data-urlencode  'page=0' \
-https://api.hh.ru/vacancies | jq '.' > hh.json
