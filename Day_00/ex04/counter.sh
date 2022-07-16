7cd230d28c6445deb673bc73ee2414730aa8e5c7
burjuly <51132559+burjuly@users.noreply.github.com>
2022-07-16 14:24:40 +0300

    Delete Day_00 directory

--- Day_00/ex04/counter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo '"name","count"' > hh_uniq_positions.csv
-
-tail -n+2 ../ex03/hh_positions.csv | 
-awk -F, '{print$3}' | sort | uniq -c | sort -r -k1,1 | 
-awk -v OFS="," '{if (index($2, "-") == 0) {print $2, $1}}' >> hh_uniq_positions.csv
--- Day_00/ex05/concatenator.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo '"id","created_at","name","has_test","alternate_url"' >> hh_positions.csv
-
-for file in 2*.csv
-do
-    tail -n +2 ${file} >> hh_positions.csv
-done
